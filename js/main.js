d83d57a1c0da2c3e13f6604b6a2e9d43c6711db8
Genie <raghava205@gmail.com>
2016-02-12 15:24:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,14 +42,15 @@ function getUserName() {
     });
   }
 function lgOut(){
-      Parse.User.logOut().then(
-            function() {
-                alert('success');
-            }, 
-            function(error) {
-                alert('error : ' + error);
-            }
-      );
+      Parse.User._currentUser._logOutWithAll();
+//      Parse.User.logOut().then(
+//            function() {
+//                alert('success');
+//            }, 
+//            function(error) {
+//                alert('error : ' + error);
+//            }
+//      );
 }
 
 (function(d, s, id){

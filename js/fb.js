e817f8586cc7590738449f66402dfe96bbec6d93
Genie <raghava205@gmail.com>
2016-02-13 11:00:58 -0500

    Update fb.js

--- js/fb.js
+++ js/fb.js
@@ -51,7 +51,11 @@ function checkLoginState() {
 function getUserName() {
     FB.api('/me?fields=id,first_name,last_name,email,gender,picture', function(response) {
       //document.getElementById('usrname').innerHTML = '<img src="http://graph.facebook.com/' + response.id + '/picture" />';;
-      document.getElementById('usrname').innerHTML = Parse.User.current().id + response.id + Parse.User.current().firstname;
+      document.getElementById('usrname').innerHTML = Parse.User.current().id + response.id;
+      if(Parse.User.current().firstname === 'undefined')
+      {
+            alert("not defined");
+      }
     });
   }
 function lgOut(){

86494b5a0c698963d91fbf5075d45047582d050c
Genie <raghava205@gmail.com>
2016-02-13 00:22:21 -0500

    Update fb.js

--- js/fb.js
+++ js/fb.js
@@ -20,9 +20,6 @@ window.fbAsyncInit = function() {
             document.getElementById("lgn-btn").style.display="none";
             document.getElementById("lgot-btn").style.display="block";
     }
-    else{
-          window.location.href = "http://www.locaholic.co";
-    }
 });
 };
 

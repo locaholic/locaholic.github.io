244344fd70ccad773f12c69e3730eaeff6192d06
Genie <raghava205@gmail.com>
2016-02-13 11:14:50 -0500

    Update fb.js

--- js/fb.js
+++ js/fb.js
@@ -52,7 +52,7 @@ function getUserName() {
     FB.api('/me?fields=id,first_name,last_name,email,gender,picture', function(response) {
       document.getElementById('usrname').innerHTML = '<img src="http://graph.facebook.com/' + response.id + '/picture" />';;
       //document.getElementById('usrname').innerHTML = Parse.User.current().id + response.id + "A";
-      if(!Parse.User.current().firstName and !Parse.User.current().lastName)
+      if(!Parse.User.current().firstName && !Parse.User.current().lastName)
       {
             alert("query");
             var usr = Parse.Object.extend("User");

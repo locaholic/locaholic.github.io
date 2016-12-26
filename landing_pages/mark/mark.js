b1f24ba1abcd3b061a79781b486be6042d072c57
sgoyal9 <sgoyal9@yahoo.com>
2016-12-26 17:18:13 +0530

    Add files via upload
    add additional form functions

--- landing_pages/mark/mark.js
+++ landing_pages/mark/mark.js
@@ -10,7 +10,7 @@ function showError(){
 }
 
 function getCode(){
-	
+
 }
 
 function leaveComment(){
@@ -24,6 +24,56 @@ function leaveComment(){
 		url: [apiHost,"registration","feedback"].join('/'),
 		data: data,
 		type: 'post',
-		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>You are Awesome!') 
+		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>You are Awesome!')
+	})
+}
+
+function leaveDetails(){
+	data = {
+		"blog_url": document.getElementById('url').value;
+		"number": document.getElementById('cellNumber').value;
+		"blogger": true;
+	  "email": document.getElementById('email').value,
+	  "name": document.getElementById('name').value,
+	  "source_page": window.location.pathname,
+	  "source_form": "details"
+	}
+	$.ajax({
+		url: [apiHost,"registration","feedback"].join('/'),
+		data: data,
+		type: 'post',
+		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Thank you for signing up.')
+	})
+}
+
+function leaveDetails2(){
+	data = {
+		"blog_url": document.getElementById('url').value;
+		"number": document.getElementById('cellNumber').value;
+		"blogger": false;
+	  "email": document.getElementById('email').value,
+	  "name": document.getElementById('name').value,
+	  "source_page": window.location.pathname,
+	  "source_form": "details2"
+	}
+	$.ajax({
+		url: [apiHost,"registration","feedback"].join('/'),
+		data: data,
+		type: 'post',
+		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Thank you for signing up.')
+	})
+}
+
+function leaveDetails3(){
+	data = {
+		"email": document.getElementById('email').value,
+	  "source_page": window.location.pathname,
+	  "source_form": "details2"
+	}
+	$.ajax({
+		url: [apiHost,"registration","feedback"].join('/'),
+		data: data,
+		type: 'post',
+		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Thank you for signing up.')
 	})
 }

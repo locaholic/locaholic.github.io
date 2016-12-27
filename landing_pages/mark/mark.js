b3327fb7070ab0e1096581beefb85d55b47eb786
Gaurav Shukla <gaurav@gauravshukla.xyz>
2016-12-27 15:25:54 +0530

    fixed bugs

--- landing_pages/mark/mark.js
+++ landing_pages/mark/mark.js
@@ -1,13 +1,27 @@
-apiHost = "https://api.locaholic.co"
+apiHost = "http://localhost:8000";
+var form_msg = {
+	"leave_comment": '<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>You are Awesome!',
+	"signup_blogger": '<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Thanks for signing up, we\'ll get back to you soon!',
+	"signup_non_blogger": '<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Welcome to the locaholic community, we\'ll get back to you soon!',
+	"waiting_list": '<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Thanks for joining the waiting list, loads of awesomeness is being created we\'d love to hear from you, reach out to us at pk@locaholic.co'
+}
+var current_form='';
 
-function showMessage(str){
-	$("#successModalBody").html(str);
+function showMessage(result,status,xhr)
+{	
+	msg = form_msg[current_form]
+	console.log(msg)
+	$("#successModalBody").html(msg);
 	$("#successModal").modal("show");
 }
 
-function showError(){
-	$("#successModalBody").html(str);
-	$("#successModal").modal("show");
+function showError(xhr,status,error){
+	str = ""
+	$.each(xhr.responseJSON, function(k, v) {
+  		str += '<p class="text-error h4"><b>'+k+" : " + v[0]+'</b></p>';
+  	});
+	$("#errorModalBody").html(str);
+	$("#errorModal").modal("show");
 }
 
 function getCode(){
@@ -15,6 +29,7 @@ function getCode(){
 }
 
 function leaveComment(){
+	current_form = "leave_comment"
 	data = {
 		"email": document.getElementById('email').value,
 		"message": document.getElementById('comment').value,
@@ -22,15 +37,17 @@ function leaveComment(){
 		"source_form": "comment"
 	}
 	$.ajax({
-		url: [apiHost,"registration","feedback"].join('/'),
-		data: data,
-		type: 'post',
-		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>You are Awesome!'),
-		error: showError('<i class="fa fa-times-circle fa-4x" aria-hidden="true"></i>Comment not submitted. Please try again.')
+		url: [apiHost,"registration","feedback/"].join('/'),
+		data: JSON.stringify(data),
+		type: 'POST',
+		success:showMessage,
+		error: showError,
+		contentType: 'application/json; charset=utf-8',
 	})
 }
 
 function signupBlogger(){
+	current_form = "signup_blogger"
 	data = {
 		"blog_url": document.getElementById('url').value,
 		"number": document.getElementById('cellNumber').value,
@@ -38,20 +55,22 @@ function signupBlogger(){
 		"email": document.getElementById('email').value,
 		"name": document.getElementById('name').value,
 		"source_page": window.location.pathname,
-		"source_form": "signupBlogger"
+		"source_form": "signupBlogger",
+		"code":"pending"
 	}
 	$.ajax({
-		url: [apiHost,"registration","forms"].join('/'),
-		data: data,
-		type: 'post',
-		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Thank you for signing up.'),
-		error: showError('<i class="fa fa-times-circle fa-4x" aria-hidden="true"></i>Oops! Please try again.')
+		contentType: 'application/json; charset=utf-8',
+		url: [apiHost,"registration","forms/"].join('/'),
+		data: JSON.stringify(data),
+		type: 'POST',
+		success: showMessage,
+		error: showError
 	})
 }
 
 function signupNonblogger(){
+	current_form = "signup_non_blogger"
 	data = {
-		"blog_url": document.getElementById('url').value,
 		"number": document.getElementById('cellNumber').value,
 		"blogger": false,
 		"email": document.getElementById('email').value,
@@ -60,25 +79,32 @@ function signupNonblogger(){
 		"source_form": "signupNonblogger"
 	}
 	$.ajax({
-		url: [apiHost,"registration","forms"].join('/'),
-		data: data,
-		type: 'post',
-		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Thank you for signing up.'),
-		error: showError('<i class="fa fa-times-circle fa-4x" aria-hidden="true"></i>Oops! Please try again.')
+		contentType: 'application/json; charset=utf-8',
+		url: [apiHost,"registration","forms/"].join('/'),
+		data: JSON.stringify(data),
+		type: 'POST',
+		success:showMessage,
+		error: showError
 	})
 }
 
-function leaveEmail(){
+function getRecommendations(){
+	current_form = "get_recommendations"
 	data = {
+		"phone": document.getElementById('cellNumber').value,
 		"email": document.getElementById('email').value,
+		"name": document.getElementById('name').value,
 		"source_page": window.location.pathname,
-		"source_form": "email"
+		"source_form": "recommendation",
+		"text": document.getElementById('preferences').value || '',
+		"location": document.getElementById('location').place_id
 	}
 	$.ajax({
-		url: [apiHost,"registration","forms"].join('/'),
-		data: data,
-		type: 'post',
-		success:showMessage('<i class="fa fa-check-circle fa-4x" aria-hidden="true"></i>Thank you for signing up.'),
-		error: showError('<i class="fa fa-times-circle fa-4x" aria-hidden="true"></i>Oops! Please try again.')
+		contentType: 'application/json; charset=utf-8',
+		url: [apiHost,"registration","ask/"].join('/'),
+		data: JSON.stringify(data),
+		type: 'POST',
+		success:showMessage,
+		error: showError
 	})
 }
